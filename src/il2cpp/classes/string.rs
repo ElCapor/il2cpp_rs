43a5a6d2d21c2926fac96e14218a651f7b21208f
PrivateMemory <104172250+ElCapor@users.noreply.github.com>
2025-11-11 19:45:02 +0100

    UnityString implement Il2CppView trait

--- src/il2cpp/classes/string.rs
+++ src/il2cpp/classes/string.rs
@@ -1,27 +1,18 @@
-use crate::il2cpp::classes::object::ObjectInner;
+use crate::{il2cpp::classes::object::ObjectInner, il2cpp_view};
 use std::{ffi::OsString, os::windows::ffi::OsStringExt, ptr::NonNull};
 
-#[repr(C)]
-pub struct Il2CppString {
-    pub obj: ObjectInner,
-    pub m_string_length: i32,
-    pub m_first_char: [u16; 32],
-}
-
-/// A zero-cost view over an IL2CPP string
-pub struct Il2CppStr<'a> {
-    inner: &'a Il2CppString,
-}
-
-impl Il2CppStr<'_> {
-    /// Create a view from a raw pointer
-    pub fn from_ptr<'a>(ptr: *mut Il2CppString) -> Option<Il2CppStr<'a>> {
-        unsafe { NonNull::new(ptr).map(|nn| Il2CppStr { inner: nn.as_ref() }) }
+il2cpp_view! {
+    pub struct UnityString {
+        pub obj: ObjectInner,
+        pub m_string_length: i32,
+        pub m_first_char: [u16; 32],
     }
+}
 
+impl UnityStringView<'_> {
     /// Length in characters
     pub fn len(&self) -> usize {
-        self.inner.m_string_length as usize
+        unsafe { self.ptr.as_ref().m_string_length as usize }
     }
 
     pub fn is_empty(&self) -> bool {
@@ -33,21 +24,13 @@ impl Il2CppStr<'_> {
         if idx >= self.len() {
             None
         } else {
-            Some(self.inner.m_first_char[idx])
+            unsafe { Some(self.ptr.as_ref().m_first_char[idx]) }
         }
     }
 
     /// Convert to a Rust `String` (UTF-16 -> UTF-8) if needed
     pub fn to_string(&self) -> String {
-        let slice = &self.inner.m_first_char[..self.len()];
+        let slice = unsafe { &self.ptr.as_ref().m_first_char[..self.len()] };
         OsString::from_wide(slice).to_string_lossy().into_owned()
     }
-
-    /// Borrow the underlying IL2CPP string directly
-    pub fn as_raw(&self) -> &Il2CppString {
-        self.inner
-    }
 }
-
-pub type UnityStringInner = Il2CppString;
-pub type UnityString<'a> = Il2CppStr<'a>;
